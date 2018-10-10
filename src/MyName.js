4ce808ab3e8b47157a46cff8da84b400413b1f4a
so0 <syngshm@gmail.com>
2018-10-10 22:31:16 +0900

    함수형 컴포넌트

--- src/MyName.js
+++ src/MyName.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 
-class MyName extends Component {
-    static defaultProps = {
-        name : '기본이름1'
-    }
-    render() {
-        return (
-            <div>
-                안녕하세요 ! 제 이름은 <b>{this.props.name}</b>입니다.
-            </div>
-        )
-    }
+// class MyName extends Component {
+//     static defaultProps = {
+//         name : '기본이름1'
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 안녕하세요 ! 제 이름은 <b>{this.props.name}</b>입니다.
+//             </div>
+//         )
+//     }
+// }
+// MyName.defaultProps = {
+//     name: '기본이름2' // 위의 내용 덮어씀
+// };
+
+// 함수형 컴포넌트
+// state와 lifecycle이 없다.
+const MyName = ({ name }) => {
+    return (
+        <div>
+            안녕하세요 제 이름은 {name} 입니다.
+        </div>
+    )
 }
-MyName.defaultProps = {
-    name: '기본이름2' // 위의 내용 덮어씀
-};
 
 export default MyName;
