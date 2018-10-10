58ceb2c16529aac0644d5129a196b5a4e3c4b5e9
so0 <syngshm@gmail.com>
2018-10-10 22:17:05 +0900

    css 클래스 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import './App.css';
 class App extends Component {
   render() {
-    const style = {
-      backgroundColor: 'black',
-      padding: '16px',
-      color: 'white',
-      fontSize: '36px'
-    };
-    return <div style={style}>안녕하세요!</div>;
+  //   const style = {
+  //     backgroundColor: 'black',
+  //     padding: '16px',
+  //     color: 'white',
+  //     fontSize: '36px'
+  //   };
+  //   return <div style={style}>안녕하세요!</div>;
+    return (
+    <div className="App">
+      리액트
+    </div>
+    );
   }
 }
 
