92858109199a731e28a36b3141ae7ba9b1577c19
so0 <syngshm@gmail.com>
2018-10-16 22:12:26 +0900

    부모 컴포넌트에 정보 전달

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm'
 class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
   render() {
     return (
       <div className="App">
-        <PhoneForm/>
+        <PhoneForm onCreate={this.handleCreate}/>
       </div>
     );
   }
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -5,19 +5,39 @@ class PhoneForm extends Component {
         name: '',
         phone: ''
     }
-    handelChange = (e) => {
+    handleChange = (e) => {
         this.setState({
             // name: e.target.value
             [e.target.name]: e.target.value
         })
     }
+    handleSubmit = (e) => {
+        // 페이지 리로딩 방지 (form submit버튼 클릭 시 자동적으로 페이지가 리로딩됨)
+        e.preventDefault();
+
+        // 상태값을 onCreate 를 통하여 부모에게 전달
+        this.props.onCreate(this.state);
+
+        this.setState({
+            name: '',
+            phone: ''
+        })
+    }
     render() {
         return (
-            <form>
-                <input placeholder="이름" name="name" value={this.state.name} onChange={this.handelChange}/>
-                <input placeholder="전화번호" name="phone" value={this.state.phone} onChange={this.handelChange} />
-                <div>{this.state.name} {this.state.phone}</div>
-
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    placeholder="이름"
+                    name="name" 
+                    value={this.state.name} 
+                    onChange={this.handleChange}/>
+                <input 
+                    placeholder="전화번호"
+                    name="phone" 
+                    value={this.state.phone} 
+                    onChange={this.handleChange} />
+                {/* <div>{this.state.name} {this.state.phone}</div> */}
+                <button type="submit">등록</button>
             </form>
         )
     }
