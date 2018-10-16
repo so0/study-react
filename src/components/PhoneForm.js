fa7fc24c9b8689dbdd4493142bcbdad616a2f457
so0 <syngshm@gmail.com>
2018-10-16 22:03:12 +0900

    phone input 추가

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 
 class PhoneForm extends Component {
     state = {
-        name: ''
+        name: '',
+        phone: ''
     }
     handelChange = (e) => {
         this.setState({
-            name: e.target.value
+            // name: e.target.value
+            [e.target.name]: e.target.value
         })
     }
     render() {
         return (
             <form>
-                <input placeholder="이름" value={this.state.name} onChange={this.handelChange}
-                />
-                <div>{this.state.name}</div>
+                <input placeholder="이름" name="name" value={this.state.name} onChange={this.handelChange}/>
+                <input placeholder="전화번호" name="phone" value={this.state.phone} onChange={this.handelChange} />
+                <div>{this.state.name} {this.state.phone}</div>
 
             </form>
         )
