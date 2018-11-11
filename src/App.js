4a53113acb07d75ffb81e1673eee26a255b42658
so0 <syngshm@gmail.com>
2018-11-11 17:53:39 +0900

    id 값 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm'
 class App extends Component {
-  
+  id = 0; // id는 렌더링 대상이 아니기 때문에 state로 넣지 않아도 된다.
   state = {
     information: []
   }
@@ -14,7 +14,20 @@ class App extends Component {
     // 비구조화 할당
     const { information } = this.state;
     this.setState({
-      information: information.concat(data)
+      // information: information.concat({
+      //   ...data, // spread 연산자
+      //   id: this.id++
+      // });
+
+      // information: information.concat({
+      //   name: data.name,
+      //   phone: data.phone,
+      //   id: this.id++
+      // });
+
+      information: information.concat(Object.assign({}, data, {
+        id: this.id++
+      }))
     })
   }
   render() {
